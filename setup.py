7cf552ba19b90e8e8a1cc8c7fafc870f2cf55a7d
bernardoamorimalvarenga <amorimbernardogame@gmail.com>
2025-05-24 15:21:44 -0300

    Changes in devenv for devm

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="devenv-manager",
-    version="0.1.0",
+    version="0.1.2",
     description="Development Environment Manager for Linux - Backup, sync and restore your dev environment in minutes",
     long_description=long_description,
     long_description_content_type="text/markdown",
